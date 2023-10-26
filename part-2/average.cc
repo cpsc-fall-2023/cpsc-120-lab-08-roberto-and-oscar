d84853cbdf0f3cfa805db5309ce605a124b0741c
Sanchez <ofsanchez696@cs101u-6e.acad.fullerton.edu>
2023-10-25 18:41:21 -0700

    l

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,7 +9,10 @@ int main(int argc, char* argv[]) {
 
   // TODO: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
-
+  if (arguments.size() != 2) {
+    std::cout << "Error: you must supply three arguments\n";
+    return 1;
+  }
   // TODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
@@ -18,11 +21,17 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
-
+  double sum{0.0};
+  double count {0.0};
+  for (int x = 0; x < arguments.size(); x++) {
+    sum += std::stoi(arguments[x]);
+    count++;
+  }
+  sum = sum / count;
   // TODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
-
+  std::cout << "average = " << sum << "\n"; 
   // TODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
